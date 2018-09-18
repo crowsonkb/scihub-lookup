20b169de15aeb59a493cfb2ff9d7a8d684e44c8c
Katherine Crowson <crowsonkb@gmail.com>
2018-09-18 08:08:10 -0700

    Edit main.js for style

--- Sci-Hub Lookup.safariextension/main.js
+++ Sci-Hub Lookup.safariextension/main.js
@@ -1,12 +1,10 @@
 function performCommand(event) {
-    if (event.command !== "button") {
-        return;
+    if (event.command == "button") {
+        var scihubDomain = safari.extension.settings.scihubDomain;
+        var url = safari.application.activeBrowserWindow.activeTab.url;
+        url = url.replace(/https?:\/\/(.+?)\//, `http://$1.${scihubDomain}/`);
+        safari.application.activeBrowserWindow.activeTab.url = url;
     }
-
-    var scihubDomain = safari.extension.settings.scihubDomain;
-    var url = safari.application.activeBrowserWindow.activeTab.url;
-    url = url.replace(/https?:\/\/(.+?)\//, `http://$1.${scihubDomain}/`);
-    safari.application.activeBrowserWindow.activeTab.url = url;
 }
 
 safari.application.addEventListener("command", performCommand, false);
